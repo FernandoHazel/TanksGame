d7ecd6a3c049db94b2d4f4bb37e58984a8010a1e
Fernando Ascencio <fernandohazel1@gmail.com>
2026-03-20 18:07:20 -0600

    car movement

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,55 +1,103 @@
+using NUnit.Framework;
 using UnityEngine;
+using System.Collections.Generic;
 using UnityEngine.InputSystem;
+using System;
+using System.Runtime.ConstrainedExecution;
 
 public class PlayerMovement : MonoBehaviour
 {
-    private Rigidbody rb;
-    private float movementX;
-    private float movementY;
-
-    [Tooltip("The speed in unity unit/second the tank move at")]
-    public float speed = 12f;                 // How fast the tank moves forward and back.
-    [Tooltip("The speed in deg/s that tank will rotate at")]
-    public float turnSpeed = 180f;            // How fast the tank turns in degrees per second.
-
-    public bool m_IsDirectControl;
-    public AudioSource m_MovementAudio;         // Reference to the audio source used to play engine sounds. NB: different to the shooting audio source.
-    public AudioClip m_EngineIdling;            // Audio to play when the tank isn't moving.
-    public AudioClip m_EngineDriving;           // Audio to play when the tank is moving.
-    public float m_PitchRange = 0.2f;           // The amount by which the pitch of the engine noises can vary.
-
-    [Tooltip("Is set to true this will be controlled by the computer and not a player")]
-    public bool m_IsComputerControlled = false; // Is this tank player or computer controlled
-
-    private string m_MovementAxisName;          // The name of the input axis for moving forward and back.
-    private string m_TurnAxisName;              // The name of the input axis for turning.
-    private Rigidbody m_Rigidbody;              // Reference used to move the tank.
-    private float m_MovementInputValue;         // The current value of the movement input.
-    private float m_TurnInputValue;             // The current value of the turn input.
-    private Vector3 m_ExplosionForceValue;      // The current value of the force  applied on the tank from an explosion.
-    private float m_OriginalPitch;              // The pitch of the audio source at the start of the scene.
-    private ParticleSystem[] m_particleSystems; // References to all the particles systems used by the Tanks
-
-    private InputAction m_MoveAction;             // The InputAction used to move, retrieved from TankInputUser
-    private InputAction m_TurnAction;             // The InputAction used to shot, retrieved from TankInputUser
-
-    private Vector3 m_RequestedDirection;       // In Direct Control mode, store the direction the user *wants* to go toward
-
-    void Start()
+    public enum Axel
     {
-        rb = GetComponent<Rigidbody>();
+        Front,
+        Rear
     }
 
-    private void OnMove(InputValue movementValue)
+    [Serializable]
+    public struct Wheel
     {
-        Vector2 movementVector = movementValue.Get<Vector2>();
-        movementX = movementVector.x;
-        movementY = movementVector.y;
+        public GameObject wheelModel;
+        public WheelCollider wheelCollider;
+        public Axel axel;
     }
 
-    void FixedUpdate()
+    PlayerControls controls;
+    Vector2 move;
+    public float speed = 12f;
+    public float maxAcceleration = 30f;
+    public float brakeAcceleration = 50f;
+    public float turnSensitivity = 1f;
+    public float maxSteerAngle = 30f;
+    public Vector3 centerOfMass;
+    public List<Wheel> wheels;
+
+    private Rigidbody carRb;
+
+    private void Awake()
     {
-        Vector3 movement = new Vector3(movementX, 0.0f, movementY);
-        rb.AddForce(movement * speed);
+        controls = new PlayerControls();
+        controls.Player.Move.performed += ctx => move =
+    ctx.ReadValue<Vector2>();
+        controls.Player.Move.canceled += ctx => move = Vector2.zero;
+    }
+
+    private void OnEnable()
+    {
+        controls.Player.Enable();
+    }
+
+    private void OnDisable()
+    {
+        controls.Player.Disable();
+    }
+    
+
+    private void Start()
+    {
+        carRb = GetComponent<Rigidbody>();
+        carRb.centerOfMass = centerOfMass;
+    }
+
+    private void Update()
+    {
+        AnimateWheels();
+    }
+
+    private void FixedUpdate()
+    {
+        Move();
+        Steer();
+    }
+
+    private void Move()
+    {
+        foreach (var wheel in wheels)
+        {
+            wheel.wheelCollider.motorTorque = move.y * maxAcceleration * Time.deltaTime;
+        }
+    }
+
+    private void Steer()
+    {
+        foreach (var wheel in wheels)
+        {
+            if (wheel.axel == Axel.Front)
+            {
+                var steerAngle = move.x * turnSensitivity * maxSteerAngle;
+                wheel.wheelCollider.steerAngle = Mathf.Lerp(wheel.wheelCollider.steerAngle, steerAngle, 0.6f);
+            }
+        }
+    }
+
+    private void AnimateWheels()
+    {
+        foreach (var wheel in wheels)
+        {
+            Quaternion rot;
+            Vector3 pos;
+            wheel.wheelCollider.GetWorldPose(out pos, out rot);
+            wheel.wheelModel.transform.position = pos;
+            wheel.wheelModel.transform.rotation = rot;
+        }
     }
 }
